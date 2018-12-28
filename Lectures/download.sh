1462f9f1e4bfe597dcd5175375c48467839d5394
hfiuza <henriquegfn@gmail.com>
2018-12-28 13:13:04 -0200

    Add script to download all lectures

---
+++ Lectures/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture01_logistics.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture01_intro.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture02_local_features.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture03_large_scale_visual_search.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture04_neural_networks.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture05_cnns_for_recognition.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture06_cnns_for_recognition_II.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture07_sparse_coding.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture08_action_recognition.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture09_human_pose.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture09_weakly_supervised.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture10_3d.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture11_rnn.pdf
+wget https://www.di.ens.fr/willow/teaching/recvis18/slides/lecture11_weakly_supervised_and_robotics.pdf
